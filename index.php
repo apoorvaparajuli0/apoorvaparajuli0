df1f03fc3dc320e1647312b2e78c6d9d78124287
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2025-04-11 20:14:09 -0700

    rewrote site using php, made some changes to meta tags, removed all javascript, redid schema.org microdata

---
+++ index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" 
+		content=
+		"
+		I am a Full Stack Software Engineer with experience working with various web frameworks including 
+		Django, Flask, Next.js, and more. I have integrated AI-enabled solutions into web applications using OpenAI's API, Anthropic's
+		Claude, and Groq. I also have academic experience and personal work involving operating systems and interpreter design and 
+		development.
+		"
+		/>
+		<meta name="keywords" content="Apoorva, Parajuli, Portfolio, Software, Engineering"/>
+		<meta name="author" content="Apoorva Parajuli"/>
+		<meta property="og:title" content="Apoorva Parajuli's Portfolio"/>
+		<meta property="og:description" 
+		content=
+		"
+		The portfolio website of Apoorva Parajuli, Full-Stack Web Developer.
+		"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content="https://apoorvaparajuli.com"/>
+		<meta property="og:image" content="https://apoorvaparajuli.com/media/me.JPG"/>
+		<meta property="og:image:type" content="image/jpg"/>
+		<meta property="og:image:alt" content="Apoorva Parajuli..."/>
+		<meta property="og:site_name" content="Apoorva's Portfolio"/>
+		<meta name="twitter:title" content="Apoorva Parajuli's Portfolio"/>
+		<meta name="twitter:card" content="summary_large_image"/>
+		<meta name="twitter:title" content="Apoorva's Portfolio"/>
+		<meta name="twitter:description" 
+		content=
+		"
+		The portfolio website of Apoorva Parajuli, Full-Stack Web Developer.
+		"/>
+		<meta name="twitter:image" content="https://apoorvaparajuli.com/media/me.JPG"/>
+		<meta name="twitter:url" content="https://apoorvaparajuli.com"/>
+		<meta http-equiv="Content-Security-Policy" 
+		content="
+		default-src 
+		'self' 
+		https://fonts.gstatic.com/; 
+		script-src 
+		'self' 
+		'unsafe-inline' 
+		https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/ 
+		https://fonts.googleapis.com/; 
+		style-src
+		'self' 
+		'unsafe-inline' 
+		https://fonts.googleapis.com/ 
+		https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/; 
+		connect-src 
+		https://apoorvaparajuli.com/sections/projects.html 
+		https://apoorvaparajuli.com/sections/professional-work.html 
+		ws://127.0.0.1:5500//ws 
+		ws://localhost:5500//ws;
+		media-src 
+		https: 
+		file: 
+		data:;
+		"/>
+		
+		<link rel="icon" href="./media/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="index.css" />
+		<title>
+			Apoorva's Portfolio
+		</title>    
+		<!--Bootstrap Imports-->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<!--Google Fonts Imports-->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<!--Bootstrap Scripts-->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div itemscope itemtype="https://schema.org/Person" class="container-fluid p-0">
+			<div class="row text-center">
+				<h2 class="text-light fw-bolder">
+					<span itemprop="givenName">
+						Apoorva
+					</span> 
+					<span itemprop="familyName">
+						Parajuli
+					</span>
+				</h2>
+				<br/>
+				<p class="contact-info text-light">
+					<a class="text-nowrap" target="_blank" href="https://linkedin.com/in/apoorva-parajuli-66b994187/">My LinkedIn</a> |
+					<a class="text-nowrap" target="_blank" itemprop="email" href="mailto:apoorvaparajuli@duck.com">apoorvaparajuli@duck.com</a> 
+					| <a class="text-nowrap" target="_blank" href="https://www.apoorvaparajuli.com/documents/Apoorva_Parajuli_Resum%C3%A9.pdf">My Resumé</a>
+					| <span class="text-nowrap" itemprop="telephone">(510) 374-9867</span> |
+					<a class="text-nowrap" target="_blank" href="https://github.com/apoorvaparajuli0">My GitHub</a>
+				</p>
+			</div>
+			<div class="row d-flex justify-content-center">
+				<div class="col-12 img-container">
+					<img itemprop="image" src="/media/me.JPG" class="img-fluid img-thumbnail" alt="Image of Apoorva Parajuli..."/>
+				</div>
+			</div>
+			<div class="mt-3 row text-center text-light">
+				<p>_________</p>
+			</div>
+			<div class="row text-light d-flex justify-content-center">
+				<div class="col-12 text-center self-bio">
+					<p itemprop="description">
+						I am a <span itemprop="jobTitle">Full Stack Software Engineer</span> with experience working with various web frameworks including 
+						Django, Flask, Next.js, and more. I have integrated AI-enabled solutions into web applications using OpenAI's API, Anthropic's
+						Claude, and Groq. I also have academic experience and personal work involving operating systems and interpreter design and 
+						development.
+					</p>
+				</div>
+			</div>
+			<div class=" row text-center text-light">
+				<p>_________</p>
+			</div>
+			<div class="row text-light text-center mt-3">
+				<h5 class="fw-bolder">Skill(s)</h5>
+			</div>
+			<div class="row">
+				<div class="col-12 px-3 text-center">
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">C</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">C++</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Python</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Java</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">JavaScript</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Vue.js</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Next.js</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">React.js</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Flask</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Django</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Chart.js</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">CMake</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">LangChain</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Auth0</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">PostgreSQL</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">OpenAI API</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Anthropic Claude</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Groq</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Google Analytics</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">AWS</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Vercel</span>
+					<span itemprop="knowsAbout" class="badge skill-badge text-bg-secondary">Heroku</span>
+				</div>
+			</div>
+			<div class="row mt-5">
+				<div class="d-none col-4 text-center">
+					<a class="work-display-link" href="https://blog.apoorvaparajuli.com/">Blog</a>
+				</div>
+				<div class="col-6 text-center">
+					<a class="work-display-link" href="/?showProf=true">Professional Work</a>
+				</div>
+				<div class="col-6 text-center">
+					<a class="work-display-link" href="/?showProf=false">Projects</a>
+				</div>
+			</div>
+			<?php if($_GET['showProf'] == "true"): ?>
+					<div class="row mt-5 text-center text-light">
+						<h3 class="fw-bolder">Professional Work</h3>
+					</div>
+					<div class="row mt-4">
+						<div class="col-12">
+							<div class="container-fluid px-3 text-light">
+								<div class="row border border-1 border-light rounded-2 work-container-box">
+									<div class="col-3 work-select-section text-center">
+										<div class="container-fluid p-0">
+											<div class="row p-0">
+												<div class="col-12 p-0">
+													<a itemprop="worksFor" href="#polypinion">Polypinion</a><br/>&#8226;<br/>
+													<a itemprop="worksFor" href="#community-support-group">Community Support Group</a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-9 work-content-section">
+										<div class="container-fluid">
+											<div class="row p-0">
+												<div class="col-12 p-0">	
+													<h5 id="polypinion" class="fw-bolder">Polypinion</h5>
+													<span class="work-links">
+														<a target="_blank" href="https://polypinion.com">Web App</a>
+													</span>
+													<p class="work-time">Apr. 2024 - Oct. 2024</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">React.js</span>
+													<span class="badge skill-badge text-bg-secondary">Flask</span>
+													<span class="badge skill-badge text-bg-secondary">AWS</span>
+													<span class="badge skill-badge text-bg-secondary">nginx</span>
+													<span class="badge skill-badge text-bg-secondary">Vercel</span>
+													<span class="badge skill-badge text-bg-secondary">PostgreSQL</span>
+													<span class="badge skill-badge text-bg-secondary">OpenAI API</span>
+													<span class="badge skill-badge text-bg-secondary">Anthropic Claude</span>
+													<span class="badge skill-badge text-bg-secondary">Google Analytics</span>
+													<ul class="work-description-list">
+														<li>
+															Developed an AI-enabled fact checker that facilitated the process 
+															of reading through news articles and parsing truth from fiction
+															using OpenAI&#39;s LLM and Anthropic&#39;s ClaudeAPI LLM
+														</li>
+														<li>
+															Developed a dashboard that took advantage of Google&#39;s Analytics API 
+															and displayed metrics that allowed admins to view the usage rate of the 
+															site and determine the most popular articles, etc. using Chart.js
+														</li>
+														<li>
+															Conducted software maintenance and deployed updates and upgrades to React.js, 
+															Flask, and SwiftUI applications
+														</li>
+														<li>
+															Performed configuration, deployment and maintenance tasks related to the application 
+															as deployed on Vercel and AWS EC2 and RDS
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="row p-0">
+												<div class="col-12 p-0">
+													<h5 id="community-support-group" itemprop="worksFor" class="fw-bolder">Community Support Group</h5>
+													<span class="work-links">
+														<a target="_blank" href="https://ncrhub.org">Web App</a>
+													</span>
+													<p class="work-time">August. 2023 - PRESENT</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">Next.js</span>
+													<span class="badge skill-badge text-bg-secondary">Django</span>
+													<span class="badge skill-badge text-bg-secondary">PostgreSQL</span>
+													<span class="badge skill-badge text-bg-secondary">AWS</span>
+													<span class="badge skill-badge text-bg-secondary">Vercel</span>
+													<span class="badge skill-badge text-bg-secondary">Heroku</span>
+													<span class="badge skill-badge text-bg-secondary">Groq</span>
+													<span class="badge skill-badge text-bg-secondary">LangChain</span>
+													<span class="badge skill-badge text-bg-secondary">nginx</span>
+													<ul class="work-description-list">
+														<li>
+															Developing an AI-enabled resource matching system, it is designed 
+															to take requests containing a user&#39;s problem/needs and address
+															them by referring them to resources contained within a database
+														</li>
+														<li>
+															Developing an application that will facilitate the community support 
+															group&#39;s operations as it relates to providing training to clients
+															and case tracking
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php elseif($_GET['showProf'] == "false"): ?>
+					<div class="row mt-5 text-center text-light">
+						<h3 class="fw-bolder">Projects</h3>
+					</div>
+					<div class="row mt-4">
+						<div class="col-12">
+							<div class="container-fluid px-3 text-light">
+								<div class="row border border-1 border-light rounded-2 work-container-box">
+									<div class="col-3 work-select-section text-center">
+										<div class="container-fluid p-0">
+											<div class="row p-0">
+												<div class="col-12 p-0">
+												<a href="#cpplox">cpplox</a><br/>&#8226;<br/>
+													<a href="#master-class-project">Master Class Project</a><br/>&#8226;<br/>
+													<a href="#inter-process-shared-memory">Inter-Process Shared Memory</a><br/>&#8226;<br/>
+													<a href="#heap-memory-allocator">Heap Memory Allocator</a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-9 work-content-section">
+										<div class="container-fluid">
+											<div class="row p-0">
+												<div class="col-12 p-0">	
+													<h5 id="cpplox" class="fw-bolder">cpplox</h5>
+													<span class="work-links">
+														<a target="_blank" href="https://github.com/apoorvaparajuli0/cpplox">GitHub</a>
+													</span>
+													<p class="work-time">Mar. 2024 - PRESENT</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">C++</span>
+													<span class="badge skill-badge text-bg-secondary">CMake</span>
+													<ul class="work-description-list">
+														<li>
+															
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="row p-0">
+												<div class="col-12 p-0">	
+													<h5 id="master-class-project" class="fw-bolder">Master Class Project</h5>
+													<p class="work-time">Jan. 2023 - June. 2023</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">Node.js</span>
+													<span class="badge skill-badge text-bg-secondary">Express</span>
+													<span class="badge skill-badge text-bg-secondary">Next.js</span>
+													<span class="badge skill-badge text-bg-secondary">socket.io</span>
+													<span class="badge skill-badge text-bg-secondary">AWS</span>
+													<span class="badge skill-badge text-bg-secondary">Vercel</span>
+													<ul class="work-description-list">
+														<li>
+															
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="row p-0">
+												<div class="col-12 p-0">	
+													<h5 id="inter-process-shared-memory" class="fw-bolder">Inter-Process Shared Memory</h5>
+													<p class="work-time">Apr. 2023</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">QEMU</span>
+													<span class="badge skill-badge text-bg-secondary">xv6</span>
+													<span class="badge skill-badge text-bg-secondary">gdb-multiarch</span>
+													<ul class="work-description-list">
+														<li>
+															
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="row p-0">
+												<div class="col-12 p-0">	
+													<h5 id="heap-memory-allocator" class="fw-bolder">Heap Memory Allocator</h5>
+													<p class="work-time">Feb. 2023 - Mar. 2023</p>
+													<p class="tools-used-title text-decoration-underline fw-bolder">Tools Used: </p>
+													<span class="badge skill-badge text-bg-secondary">QEMU</span>
+													<span class="badge skill-badge text-bg-secondary">xv6</span>
+													<span class="badge skill-badge text-bg-secondary">gdb</span>
+													<ul class="work-description-list">
+														<li>
+															
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			<?php endif; ?>
+		</div>
+	</body>
+</html>
