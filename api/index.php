fe7bbec70806626e2cc00d7ea07227078c0fcb3e
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2025-04-15 17:14:57 -0700

    added a og:site_name tag to maybe fix an issue with html parsing?

--- api/index.php
+++ api/index.php
@@ -15,6 +15,7 @@
 		/>
 		<meta name="keywords" content="Apoorva, Parajuli, Portfolio, Software, Engineering"/>
 		<meta name="author" content="Apoorva Parajuli"/>
+		<meta property="og:site_name" content="Apoorva Parajuli's Portfolio"/>
 		<meta property="og:title" content="Apoorva Parajuli's Portfolio"/>
 		<meta property="og:description" 
 		content=
@@ -61,7 +62,7 @@
 		<link rel="icon" href="/media/favicon.ico">
 		<link rel="stylesheet" type="text/css" href="/css/index.css" />
 		<title>
-			Apoorva's Portfolio
+			Apoorva Parajuli's Portfolio
 		</title>    
 		<!--Bootstrap Imports-->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
