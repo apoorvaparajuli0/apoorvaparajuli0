34804b48e47e0034de609fdb7b278548b7d91b29
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2025-04-26 03:33:17 -0700

    removed API paths from main

--- api/index.php
+++ api/index.php
@@ -182,7 +182,7 @@
 			<div id="bottom" class="row text-center mt-5">
 				<h2 class="text-light work-section-title">
 					<?php if(array_key_exists("showEd", $_GET) && $_GET['showEd'] == "true"): ?>
-						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/api/?showProj=true#bottom">
+						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/?showProj=true#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-left d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"/>
 							</svg>
@@ -190,13 +190,13 @@
 						<p class="fw-bold d-inline">
 							Education
 						</p>
-						<a class="text-decoration-none icon-link-hover" href="/api/#bottom">
+						<a class="text-decoration-none icon-link-hover" href="/#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-right d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
 							</svg>
 						</a>
 					<?php elseif(!(array_key_exists("showProj", $_GET) || array_key_exists("showEd", $_GET))): ?>
-						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/api/?showEd=true#bottom">
+						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/?showEd=true#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-left d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"/>
 							</svg>
@@ -204,13 +204,13 @@
 						<p class="fw-bold d-inline">
 							Experience
 						</p>
-						<a class="text-decoration-none icon-link-hover" href="/api/?showProj=true#bottom">
+						<a class="text-decoration-none icon-link-hover" href="/?showProj=true#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-right d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
 							</svg>
 						</a>
 					<?php elseif(array_key_exists("showProj", $_GET) && $_GET['showProj'] == "true"): ?>
-						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/api/#bottom">
+						<a class="text-decoration-none icon-link-hover" style="--bs-icon-link-transform: translate3d(-.375rem, 0, 0);" href="/#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-left d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"/>
 							</svg>
@@ -218,7 +218,7 @@
 						<p class="fw-bold d-inline">
 							Projects
 						</p>
-						<a class="text-decoration-none icon-link-hover" href="/api/?showEd=true#bottom">
+						<a class="text-decoration-none icon-link-hover" href="/?showEd=true#bottom">
 							<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-bar-right d-inline swap-work-button" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
 							</svg>
