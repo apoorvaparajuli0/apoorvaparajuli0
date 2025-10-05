86b9aa24fb438dd9135250d12b661f5afdf677c8
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2025-10-05 14:00:20 -0700

    updated resume, meta description and linkedin link, and email

--- api/index.php
+++ api/index.php
@@ -22,19 +22,19 @@
 		content=
 		"
 		I am a Full Stack Software Engineer with experience working with various web frameworks including 
-		Django, Flask, Next.js, and more. I have integrated AI-enabled solutions into web applications using OpenAI's API, Anthropic's
-		Claude, and Groq. I also have academic experience and personal work involving operating systems and interpreter design and 
-		development.
+		Django, Flask, React, Next.js, and more. I have experience integrating third party API's, and
+		orchestration tools such as LangChain as well as developing secure robust, and accessible web
+		applications.
 		"
 		/>
-		<meta name="keywords" content="Apoorva, Parajuli, Portfolio, Software, Engineering"/>
+		<meta name="keywords" content="Apoorva, Parajuli, Portfolio, Software, Engineering, Full-Stack, Backend, Frontend"/>
 		<meta name="author" content="Apoorva Parajuli"/>
 		<meta property="og:site_name" content="Apoorva Parajuli's Portfolio"/>
 		<meta property="og:title" content="Apoorva Parajuli's Portfolio"/>
 		<meta property="og:description" 
 		content=
 		"
-		The portfolio website of Apoorva Parajuli, Full-Stack Web Developer.
+		The portfolio website of Apoorva Parajuli, Full-Stack Software Engineer.
 		"/>
 		<meta property="og:type" content="website"/>
 		<meta property="og:url" content="https://apoorvaparajuli.com"/>
@@ -48,7 +48,7 @@
 		<meta name="twitter:description" 
 		content=
 		"
-		The portfolio website of Apoorva Parajuli, Full-Stack Web Developer.
+		The portfolio website of Apoorva Parajuli, Full-Stack Software Engineer.
 		"/>
 		<meta name="twitter:image" content="https://apoorvaparajuli.com/assets/me.JPG"/>
 		<meta name="twitter:url" content="https://apoorvaparajuli.com"/>
@@ -77,13 +77,13 @@
 					"name": "Apoorva Parajuli",
 					"description": 
 					"I am a Full Stack Software Engineer with experience working with various web frameworks including 
-					Django, Flask, Next.js, and more. I have integrated AI-enabled solutions into web applications using OpenAI's API, Anthropic's
-					Claude, and Groq. I also have academic experience and personal work involving operating systems and interpreter design and 
-					development.",
+					Django, Flask, React, Next.js, and more. I have experience integrating third party API's, and
+					orchestration tools such as LangChain as well as developing secure robust, and accessible web
+					applications.",
 					"image": "https://apoorvaparajuli.com/assets/me.JPG",
 					"sameAs": [
 					"https://github.com/apoorvaparajuli0",
-					"https://linkedin.com/in/apoorva-parajuli-66b994187"
+					"https://linkedin.com/in/apoorva-parajuli"
 					]
 				}
 			}
@@ -128,8 +128,8 @@
 							</div>
 							<div class="row">
 								<p class="set-text-color contact-info">
-									<span class="cursor-pointer text-decoration-underline text-nowrap profile-link ms-1 me-1 mb-2" id="email-link" itemprop="email">apoorvaparajuli@duck.com</span> | 
-									<a class="text-nowrap profile-link ms-1 me-1 mb-2" rel="noreferrer noopener" target="_blank" href="https://apoorvaparajuli.com/assets/Apoorva_Parajuli_Resum%C3%A9.pdf">My Resumé</a> |
+									<span class="cursor-pointer text-decoration-underline text-nowrap profile-link ms-1 me-1 mb-2" id="email-link" itemprop="email">apoorvaparajuli0@gmail.com</span> | 
+									<a class="text-nowrap profile-link ms-1 me-1 mb-2" rel="noreferrer noopener" target="_blank" href="https://apoorvaparajuli.com/assets/Apoorva_Parajuli_Resume.pdf">My Resumé</a> |
 									<span class="cursor-pointer text-decoration-underline text-nowrap profile-link ms-1 me-1 mb-2" id="phone-link" itemprop="telephone">(510) 374-9867</span>
 								</p>
 							</div>
@@ -152,7 +152,7 @@
 							</div>
 							<div class="row">
 								<div class="col-12">
-									<a target="_blank" rel="noreferrer noopener" class="text-decoration-none" href="https://www.linkedin.com/in/apoorva-parajuli-66b994187/">
+									<a target="_blank" rel="noreferrer noopener" class="text-decoration-none" href="https://www.linkedin.com/in/apoorva-parajuli/">
 									<svg aria-label="LinkedIn Profile Icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="me-1 cursor-pointer bi bi-linkedin set-text-color" viewBox="0 0 16 16">
 										<title>LinkedIn Profile</title>
 										<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
